41532d8374f8a52d3fc7bc3d89c099d393e3a4f8
ItsAbubakarShf <itsabubakarshf@gmail.com>
2023-12-20 22:17:52 +0500

    01-js/easy/anagram completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,7 +4,20 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
+/*
+1. check len if equal
+2. if len is equal than start checking if words of str1 exist in str2
+*/
+
 function isAnagram(str1, str2) {
+  if (str1.length != str2.length) {
+    return false;
+  }
+  // normalize and sorted the strings
+  const sortedStr1=str1.toLowerCase().split('').sort().join()
+  const sortedStr2=str2.toLowerCase().split('').sort().join()
+ 
+  return sortedStr1===sortedStr2
 
 }
 
